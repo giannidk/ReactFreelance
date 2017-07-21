41853e04a85849127ff748003dfe17235e58cf8e
Gianni <g.ambro@gmail.com>
2017-07-21 03:26:02 +0200

    dashboard explanations edited

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -12,16 +12,19 @@ class Dashboard extends Component {
                     This demo app doesn't have an implemented authentication.
                 </ListGroupItem>
                 <ListGroupItem header="Settings">
-                    Settings such as VAT value and currency are provided at ap start and can't be changed.
+                    Settings such as VAT value and currency are provided at app start and can't be changed.
                 </ListGroupItem>
                 <ListGroupItem header="Clients">
-                    Clients are already set, there's possibility to add and remove clients but not to edit specific client.
+                    Clients are already set, it's possible to add and remove clients but not to edit specific client.
                 </ListGroupItem>
                 <ListGroupItem header="Projects">
-                    Projects are already set in database, no adding and removing functionalityes.
+                    Projects are already set in database, no adding and removing functionalities.
                 </ListGroupItem>
                 <ListGroupItem header="Registrations">
                     Registrations can be added and deleted. The status of the registrations will change when they are invoiced.                  
+                </ListGroupItem>
+                <ListGroupItem header="Invoices">
+                    Invoices can be created on project basis and include registrations which are not yet being invoiced. When a registration is invoiced its status is set to invoiced and it won't be included in new invoices.                 
                 </ListGroupItem>
             </ListGroup>
             </div>
